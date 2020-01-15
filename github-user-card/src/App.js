40882dc6149216c47eebcf0de6e0d2275f616d21
Chris Engel <Chris.Engel@yahoo.com>
2020-01-14 20:54:46 -0500

    mvp should be done after small bug is fixed

---
+++ github-user-card/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Axios from 'axios';
+import './App.css';
+import FollowerList from './components/followerList';
+import UserCard from './components/userCard';
+
+class App extends React.Component {
+  
+  state = {
+    myGithub: [],
+    myFollowers: []
+  }
+
+  componentDidMount(){
+    Axios
+      .get('https://api.github.com/users/Dazmen')
+      .then(res => {
+        console.log('My Github API', res);
+        this.setState({myGithub: res.data})
+      })
+      .catch(err => {
+        console.log('User data was not recieved', err)
+      })
+      Axios
+      .get('https://api.github.com/users/Dazmen/followers')
+      .then(res => {
+        console.log('Followers Array', res);
+        this.setState({myFollowers: res.data})
+      })
+      .catch(err => {
+        console.log('Follower data was note recieved', err)
+      })
+  }
+
+  render(){
+    console.log(this.state.myFollowers)
+    return (
+      <div>
+        <header>
+          <h1>Github-User-Card</h1>
+          <h2>This time, with React!</h2>
+        </header>
+        <div>
+          <UserCard myGithub={this.state.myGithub}/>
+          {/* <FollowerList myFollowers={this.state.myFollowers}/> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ github-user-card/src/components/followerList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FollowerCard from './followerCard'
+
+// const FollowerList = ({myFollowers}) => {
+//     console.log(myFollowers);
+//     return(
+//         <div>
+//             {myFollowers.map(item => {
+//                 <FollowerCard 
+//                 key={item.id}
+//                 myFollowers={myFollowers}
+//                 />        
+//             })}
+//         </div>
+//     ) 
+// }
+
+// export default FollowerList
